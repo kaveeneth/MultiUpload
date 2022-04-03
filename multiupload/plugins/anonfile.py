f0e2a36a79eca8e858229dd5a1d921bd01559a00
kaveeneth <93315162+kaveeneth@users.noreply.github.com>
2022-04-03 21:06:06 +0530

    Update anonfile.py

--- multiupload/plugins/anonfile.py
+++ multiupload/plugins/anonfile.py
@@ -68,7 +68,7 @@ Server: AnonFile
 NOTE: Cant find notes. Its also anonymous 🤕'''
 	await msg.edit(hmm, buttons=(
 		[Button.url('📦 Download', r.json()["data"]["file"]["url"]["short"])],
-		[Button.url('Support Chat 💭', 't.me/harp_chat')]
+		[Button.url('Support Chat 💭', 't.me/KN_URL_Support')]
 		))
 
 	os.remove(result.name)
